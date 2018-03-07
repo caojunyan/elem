b7ec11c485bdf8c1eab3c6f0f66b9ae420ffc94f
caojunyan <2433027313@qq.com>
2018-03-07 22:25:37 +0800

    d

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,50 +1,77 @@
 <template>
-<div class="header">
-  <div class="content-wrapper">
-    <div class="avatar">
-      <img :src="seller.avatar" width="64" height="64" alt="">
-    </div>
-    <div class="content">
-      <div class="title">
-        <span class="brand"></span>
-        <span class="name">{{seller.name}}</span>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img :src="seller.avatar" width="64" height="64" alt="">
       </div>
-      <div class="description">
-        {{seller.description}}/{{seller.deliveryTime}}分钟送达
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div v-if="seller.supports" class="support">
+          <div class="icon" :class="classMap[seller.supports[0].type]"></div>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
       </div>
-      <div v-if="seller.supports" class="support">
-        <div class="icon" :class="classMap[seller.supports[0].type]"></div>
-        <span class="text">{{seller.supports[0].description}}</span>
-      </div>
-    </div>
-    <div v-if="seller.supports" class="support-count" @click="showDetail">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
       <span class="count">
         {{seller.supports.length}}
       </span>
-      <i class="icon-keyboard_arrow_right"></i>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-  </div>
-  <div class="bulletin-wrapper " @click="showDetail">
+    <div class="bulletin-wrapper " @click="showDetail">
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
-  </div>
-  <div class="background">
-    <img :src="seller.avatar" alt="" width="100%" height="134">
-  </div>
-  <div class="detail" v-show="detailShow">
-    <div class="detail-wrapper clearfix">
-      <div class="detail-main"></div>
     </div>
-    <div class="detail-close">
-      <i class="icon-close"></i>
+    <div class="background">
+      <img :src="seller.avatar" alt="" width="100%" height="134">
     </div>
+    <transition name="fade">
+      <div class="detail" v-show="detailShow">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item,index) in seller.supports">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
+        </div>
+      </div>
+    </transition>
   </div>
-</div>
 
 </template>
 
 <script>
-export default {
+  import star from '../star/star.vue'
+  export default {
     props:{
       seller: {
         type: Object
@@ -55,25 +82,31 @@ export default {
         detailShow:false
       }
     },
-  methods:{
-    showDetail(){
-      this.detailShow=true
-    }
-  },
+    methods:{
+      showDetail(){
+        this.detailShow=true
+      },
+      hideDetail(){
+        this.detailShow=false
+      }
+    },
     created(){
       this.classMap=['decrease','discount','guarantee','invoice','special']
-  }
+    },
+    components:{
+      star
+    }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
-.header
-  color: #ffffff
-  background rgba(7,17,27,.5)
-  overflow hidden
-  .content-wrapper
+  .header
+    color: #ffffff
+    background rgba(7,17,27,.5)
+    overflow hidden
+    .content-wrapper
       padding 24px 12px 18px 24px
       font-size 0
       position relative
@@ -151,61 +184,131 @@ export default {
 
 
 
-  .bulletin-wrapper
-    height 28px
-    position relative
-    line-height 28px
-    padding  0 22px 0 12px
-    white-space nowrap
-    overflow hidden
-    text-overflow ellipsis
-    background rgba(7,17,27,.4)
-    .bulletin-title
-      display inline-block
-      vertical-align top
-      margin-top 9px
-      width 22px
-      height 12px
-      bg-image('bulletin')
-      background-size 22px 12px
-      background-repeat no-repeat
-    .bulletin-text
-      font-size 10px
-      margin 0 4px
-    .icon-keyboard_arrow_right
-      position absolute
-      font-size 10px
-      right 12px
-      top:8px
+    .bulletin-wrapper
+      height 28px
+      position relative
+      line-height 28px
+      padding  0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      background rgba(7,17,27,.4)
+      .bulletin-title
+        display inline-block
+        vertical-align top
+        margin-top 9px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+        font-size 10px
+        margin 0 4px
+      .icon-keyboard_arrow_right
+        position absolute
+        font-size 10px
+        right 12px
+        top:8px
 
-  .background
-    position absolute
-    top: 0
-    left:0
-    width 100%
-    height 100%
-    z-index -1
-    filter blur(10px)
-  .detail
-    width 100%
-    position fixed
-    z-index 100
-    height 100%
-    overflow auto
-    background rgba(7,17,27,.8)
-    top 0
-    left 0
-    .detail-wrapper
-      min-height 100%
-      .detail-main
-        margin-top 64px;
-        padding-bottom 64px
-     .detail-close
-       position relative
-       width 32px
-       height 32px
-       margin -64px auto 0 auto
-       clear both
-       font-size 32px
+    .background
+      position absolute
+      top: 0
+      left:0
+      width 100%
+      height 100%
+      z-index -1
+      filter blur(10px)
+    .detail
+      width 100%
+      position fixed
+      z-index 100
+      height 100%
+      overflow auto
+      top 0
+      left 0
+      transition all .5s
+      background-filter 10px
+      &.fade-transition
+        opacity 1
+        background rgba(7,17,27,.8)
+      &.fade-enter,&.fade-leave
+        opacity 1
+        background rgba(7,17,27,.8)
+      .detail-wrapper
+        min-height 100%
+        width 100%
+        .detail-main
+          margin-top 64px;
+          padding-bottom 64px
+          .name
+            line-height 16px
+            text-align: center
+            font-size 16px
+            font-weight 700
+          .star-wrapper
+            margin-top 18px
+            padding 2px 0
+            text-align:center
+          .title
+            width 80%
+            margin 28px auto 24px auto
+            display flex
+            .line
+              flex 1
+              position relative
+              top:-6px
+              font-weight 700
+              border-bottom 1px solid rgba(255,255,255,.2)
+            .text
+              padding 0 12px
+              font-size 14px
+          .supports
+            width 80%
+            margin  0 auto
+            .support-item
+              padding 0 12px;
+              margin-bottom 12px;
+              font-size 0
+              &:last-child
+                margin-bottom 0
+                .icon
+                  display inline-block
+                  width 16px
+                  height 16px
+                  vertical-align top
+                  margin-right 6px
+                  background-size 16px 16px
+                  background-repeat no-repeat
+                  &.decrease
+                    bg-image('decrease_2')
+                  &.discount
+                    bg-image('discount_2')
+                  &.guarantee
+                    bg-image('guarantee_2')
+                  &.invoice
+                    bg-image('invoice_2')
+                  &.special
+                    bg-image('special_2')
+
+
+                .text
+                  font-size 12px
+                  line-height 16px
+
+          .bulletin
+            width 80%
+            margin 0 auto
+            .content
+              padding  0 12px
+              line-height 24px
+              font-size 12px
+    .detail-close
+      position relative
+      width 32px
+      height 32px
+      margin -64px auto 0 auto
+      clear both
+      font-size 32px
 
 </style>
